da7b154f7def884a7dafc8000d1d288d7c3a485e
Minmin77-lab <panahovaarina52@gmail.com>
2025-02-23 17:33:57 +0500

    Update tok1.sol

--- tok1.sol
+++ tok1.sol
@@ -3,20 +3,24 @@ pragma solidity ^0.8.0;
 
 import "./ERC20.sol"; 
 
-contract MyToken is ERC20 {
+contract YourToken is ERC20 {
     address private owner;
 
-    constructor() ERC20("MyToken", "MTK", 1000000 * 10 ** 12, 1 ether) { //начальное количество токенов
+    constructor() ERC20("YourToken", "YTK", 1000000 * 10 ** 18, 1 ether) { //начальное количество токенов
         owner = msg.sender; // владелец контракта
     }
 
-    modifier onlyOwner() {
+    modifier onlyOwner() override {
         require(msg.sender == owner, "Caller is not the owner");
         _;
     }
 
     function mint(address to, uint256 amount) external onlyOwner {
         _mint(to, amount);
+    }
+
+    function price() external view returns (uint256) {
+        return 3 ether / 10**decimals();
     }
 
     function burn(uint256 amount) public override {
